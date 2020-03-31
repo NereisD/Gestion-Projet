1d9c0bec7aa8f90791d5251e2eb68c93de79f662
laroche28 <florian.laroche1@etu.unilim.fr>
2020-03-31 12:57:50 +0200

    map fonctionnel avec adresses des fournisseurs et de la livraison

--- js/api.js
+++ js/api.js
@@ -102,18 +102,6 @@ async function test() {
     console.log("ID des commandes du jours");
     console.log(idCommandesDuJour);
 
-    //Pour accéder aux adresses de toutes les commandes du jour 
-    let adressesLivraisonToutesCommandesDuJour = [];
-    let i = 0;
-    $.each(idCommandesDuJour, function (key, val) {
-    getAdresseLivraisonId(val).then(function (adrr) {
-        adressesLivraisonToutesCommandesDuJour[i] = adrr;
-        i++
-    });
-    })
-    console.log("Adresse de livraison de toutes les commandes du jour");
-    console.log(adressesToutesCommandesDuJour);
-
     //Affichage de l'adresse de livraison d'une commande
     let adresseLivraison = await getAdresseLivraisonId(idCommandesDuJour[8]);
     console.log("Adresse de livraison d'une commande passé en paramètre");
@@ -124,4 +112,4 @@ async function test() {
     console.log("Adresses des fournisseurs d'une commande passé en paramètre");
     console.log(adressesFournisseur);
 }
-test();
+//test();
